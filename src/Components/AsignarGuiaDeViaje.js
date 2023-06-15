eaa0f8c348507b7ee62ca27ffae654f0a465a0d0
Naren Biardo <70331736+narenbiardo@users.noreply.github.com>
2023-06-15 18:40:18 -0300

    Now guiasDeViaje are getted from the api

--- src/Components/AsignarGuiaDeViaje.js
+++ src/Components/AsignarGuiaDeViaje.js
@@ -1,11 +1,13 @@
-import { useState } from "react";
-import TextInput from "react-autocomplete-input";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { RESTEndpoints } from "../Services/RestService";
 import "react-autocomplete-input/dist/bundle.css";
 import { FormDiv } from "../Utilities/FormDiv";
 import { FormH2 } from "../Utilities/FormH2";
 import { FormSelect } from "../Utilities/FormSelect";
 import { FormTextInputAutocomplete } from "../Utilities/FormTextInputAutocomplete";
 import { FormInputSubmit } from "../Utilities/FormInputSubmit";
+import { FormInputDiv } from "../Utilities/FormInputDiv";
 
 const vehiculos = [
 	"ABC1111",
@@ -23,11 +25,13 @@ const choferes = [
 	{ id: "789", nombre: "Lucas" },
 ];
 
+/*
 const guiasDeViaje = [
 	{ id: "1", nombre: "Guia 1" },
 	{ id: "2", nombre: "Guia 2" },
 	{ id: "3", nombre: "Guia 3" },
 ];
+*/
 
 class AsignarGuiaViajeForm {
 	constructor(idGuiaViaje, cedulaChofer, matriculaVehiculo) {
@@ -39,6 +43,8 @@ class AsignarGuiaViajeForm {
 
 export const AsignarGuiaDeViaje = () => {
 	const [agvf, setAgvf] = useState(new AsignarGuiaViajeForm());
+	const [guiasDeViaje, setGuiasDeViaje] = useState([]);
+	//const [vehiculos, setVehiculos] = useState([]);
 
 	const handleChangeAgvf = e => {
 		if (e.target) {
@@ -49,6 +55,37 @@ export const AsignarGuiaDeViaje = () => {
 			setAgvf(prevData => ({ ...prevData, ["matriculaVehiculo"]: e }));
 		}
 	};
+
+	const handleGuiasDeViaje = () => {
+		axios
+			.get(RESTEndpoints.guiasViajesService.listarGuiasAsignables)
+			.then(response => {
+				//console.log(response.data);
+				setGuiasDeViaje(response.data);
+			})
+			.catch(error => {
+				console.log(error);
+			});
+	};
+
+	/*
+	const handleVehiculos = () => {
+		axios
+			.get(RESTEndpoints.encargadoService.vehiculos)
+			.then(response => {
+				console.log(response.data);
+				setVehiculos(response.data);
+			})
+			.catch(error => {
+				console.log(error);
+			});
+	};
+	*/
+
+	useEffect(() => {
+		handleGuiasDeViaje();
+		//handleVehiculos();
+	}, []);
 
 	return (
 		<FormDiv>
@@ -87,11 +124,45 @@ export const AsignarGuiaDeViaje = () => {
 						>
 							Volver
 						</button>
-
 					</div>
 				</>
 			) : (
-				<FormSelect
+				<FormInputDiv>
+					<label htmlFor="idGuiaViaje">Guía de Viaje</label>
+					<select
+						name="idGuiaViaje"
+						form="idGuiaViajeForm"
+						onChange={handleChangeAgvf}
+						defaultValue=""
+						required
+						style={{
+							marginLeft: "10px",
+							padding: "5px",
+							border: "none",
+							borderBottom: "2px solid #16b7b9",
+							width: "250px",
+							fontSize: "16px",
+							color: "#555",
+						}}
+					>
+						<option value="" disabled>
+							"Seleccionar Guía de Viaje"
+						</option>
+						{guiasDeViaje.map(gv => (
+							<option value={gv.idGuiaViaje} key={Math.random()}>
+								{"Calle: " +
+									gv.destino.calle +
+									", Km: " +
+									gv.destino.km +
+									", Nº Puerta: " +
+									gv.destino.nroPuerta +
+									", Fecha: " +
+									new Date(gv.fechaHora).toLocaleString()}
+							</option>
+						))}
+					</select>
+				</FormInputDiv>
+				/*<FormSelect
 					htmlFor="idGuiaViaje"
 					label="Guía de Viaje"
 					name="idGuiaViaje"
@@ -99,7 +170,7 @@ export const AsignarGuiaDeViaje = () => {
 					onChangeHandler={handleChangeAgvf}
 					optionDisabled="Seleccionar Guía de Viaje"
 					valueArray={guiasDeViaje}
-				/>
+				/>*/
 			)}
 		</FormDiv>
 	);
