ad39b5e7e9038376156a92a4a7902410f89cb1f6
mrestre95 <mauroc2006@hotmail.com>
2023-06-21 12:44:56 -0300

    arreglos en estilos y popup

--- src/constants.js
+++ src/constants.js
@@ -6,6 +6,7 @@ import {
 } from "./classes";
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
+import Button from '@mui/material/Button';
 
 export const serverURL = "http://localhost:8080/";
 
@@ -58,23 +59,41 @@ export const ftiaef = new FirstTimeInputAñadirEmpresaForm( //used to check if t
 );
 
 export const columnsVehiculos = [
-	{ field: "matricula", headerName: "Matricula", width: 100 },
-	{ field: "marca", headerName: "Marca", width: 150 },
-	{ field: "modelo", headerName: "Modelo", width: 100 },
+	{ field: "matricula", headerName: "Matricula", flex:1 },
+	{ field: "marca", headerName: "Marca", flex:1 },
+	{ field: "modelo", headerName: "Modelo", flex:1 },
+	{ field: "capacidad", headerName: "Capacidad", flex:1 },
 ];
 
 export const columnsGuiasDeViaje = [
-	{ field: "id", headerName: "Id", width: 10 },
-	{ field: "rubro", headerName: "Rubro", width: 150 },
-	{ field: "kmOrigen", headerName: "Km Origen", width: 100 },
-	{ field: "kmDestino", headerName: "Km Destino", width: 100 },
-	{ field: "fecha", headerName: "Fecha", width: 90 },
-	{ field: "hora", headerName: "Hora", width: 80 },
+	{ field: "id", headerName: "Id", flex:1},
+	{ field: "rubro", headerName: "Rubro", flex:1 },
+	{ field: "origen", headerName: "Origen", flex:1 },
+	{ field: "destino", headerName: "Destino", flex:1 },
+	{ field: "fecha", headerName: "Fecha", flex:1 },
+	{ field: "hora", headerName: "Hora", flex:1 },
+	{ field: "estadoViaje", headerName: "Estado", flex:1 },
+	{ field: "volumenCarga", headerName: "Volumen Carga", flex:1 },
+	{
+		field: 'asignar',
+		headerName: '',
+		flex:1,
+		renderCell: (params) => (
+		  <Button variant="text" className="asignar-btn"
+		  >
+			ASIGNAR
+		  </Button>
+		),
+	  },
+
 ];
 
 export const columnsChoferes = [
-	{ field: "id", headerName: "Id", width: 10 },
-	{ field: "nombre", headerName: "Nombre", width: 150 },
+	{ field: "nombre", headerName: "Nombre", flex:1 },
+	{ field: "apellido", headerName: "Apellido", flex:1 },
+	{ field: "cedula", headerName: "Cedula", flex:1 },
+	{ field: "email", headerName: "Email", flex:1 },
+	{ field: "vencimientoLicencia", headerName: "Venc. Licencia", flex:1 },
 ];
 
 export const columnsVehiculosFull = [
