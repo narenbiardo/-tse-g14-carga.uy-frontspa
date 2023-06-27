3596992ed6602c0aab1dee81d04cb8f38caa8842
Naren Biardo <70331736+narenbiardo@users.noreply.github.com>
2023-06-26 21:46:57 -0300

    RedirectUri changed

--- src/Services/settings-code.js
+++ src/Services/settings-code.js
@@ -4,7 +4,8 @@ export const responseType = "code";
 export const scope = "openid%20personal_info%20email%20document";
 export const clientId = "890192";
 export const state = "SPA";
-export const redirectUri = "http://localhost:8080";
+export const redirectUri =
+	"https://carga-uy-back.web.elasticloud.uy/api/callback/login";
 
 export const appServerUrl = "https://auth-testing.iduruguay.gub.uy";
 export const oauthServerUrl = "123";
